1d19c40cac598519d4160eb9987fb7f4ef4c73b6
Vit <vit_waladzko@mail.ru>
2021-10-31 17:34:17 +0300

    tasks 31.10

---
+++ tasks.js
@@ -0,0 +1,28 @@
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
+  console.log(greet('Alex'));
+  
+
+  function makeNegative(num) {
+    if(num === 0 || num < 0){
+      return num;
+      }else{
+        return -num
+      }
+    }
+  console.log(makeNegative(1));
+  console.log(makeNegative(-5));
+  console.log(makeNegative(0));
+  console.log(makeNegative(0.12));
+  
+  
+  function boolToWord(bool){
+    if (bool===true) {
+      return 'Yes';
+    } else {
+      return 'No';
+    }
+  }
+  console.log(boolToWord(true));
+  console.log(boolToWord(false));
