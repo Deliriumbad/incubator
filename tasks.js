afa7bd0777da5719afbe85708690a480a9e3e7c2
Vit <vit_waladzko@mail.ru>
2021-11-01 22:39:36 +0300

    01.11/2

--- tasks.js
+++ tasks.js
@@ -62,3 +62,52 @@ function greet(name){
   }
   console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]))//21
   console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6]))//-21
+
+  
+  
+  function even_or_odd(number) {
+    if(number%2===0){
+      return 'Even';
+    }else{
+      return 'Odd';
+    }
+  }
+  console.log(even_or_odd(1));
+  console.log(even_or_odd(6));
+  console.log(even_or_odd(-71));
+
+  
+  
+  function findSmallestInt(args) {
+      return Math.min.apply(null, args);
+    }
+  console.log(findSmallestInt([-1, -17, -53]));
+  console.log(findSmallestInt([5, -17, 11]));
+
+
+  function stringToArray(string){
+	return string.split(' ');
+}
+console.log(stringToArray('Hi world'));
+
+
+function updateLight(current) {
+    if (current === "green"){
+      return "yellow";
+    }
+     if (current === "yellow"){
+      return "red";
+    }
+     if (current === "red"){
+      return "green";
+    }
+   }
+   console.log(updateLight('green'));
+   console.log(updateLight('yellow'));
+   console.log(updateLight('red'));
+
+
+   function reverseWords(str){
+    return str.split(' ').reverse().join(' ');
+  }
+  console.log(reverseWords('hello world!'));
