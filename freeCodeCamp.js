ddb546fba5be64feef499b2712afb0dd98c7c6b4
Vit <vit_waladzko@mail.ru>
2021-11-02 10:49:09 +0300

    02.11/2

---
+++ freeCodeCamp.js
@@ -0,0 +1,33 @@
+let quotient = 4.4 / 2;//Divide One Decimal by Another with JavaScript
+
+const remainder = 11 % 3;//Finding a Remainder in JavaScript
+
+let a = 3;
+let b = 17;
+let c = 12;
+a += 12;
+b += 9;
+c += 7;//Compound Assignment With Augmented Addition
+
+let a = 11;
+let b = 9;
+let c = 3;
+a = a - 6;
+b = b - 15;
+c = c - 1;//Compound Assignment With Augmented SubtractionPassed
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+a *= 5;
+b *= 3;
+c *= 10;//Compound Assignment With Augmented Multiplication
+
+let a = 48;
+let b = 108;
+let c = 33;
+a /= 12;
+b /= 4;
+c /= 11;//Compound Assignment With Augmented Division
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; //Escaping Literal Quotes in Strings
