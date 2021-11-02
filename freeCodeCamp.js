4a7889f3da0d067fbb9f979bfb771e1c738e0f46
Vit <vit_waladzko@mail.ru>
2021-11-02 10:52:09 +0300

    02.11/2

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -9,25 +9,25 @@ a += 12;
 b += 9;
 c += 7;//Compound Assignment With Augmented Addition
 
-let a = 11;
-let b = 9;
-let c = 3;
-a = a - 6;
-b = b - 15;
-c = c - 1;//Compound Assignment With Augmented SubtractionPassed
+let a1 = 11;
+let b1 = 9;
+let c1 = 3;
+a1 = a1 - 6;
+b1 = b1 - 15;
+c1 = c1 - 1;//Compound Assignment With Augmented SubtractionPassed
 
-let a = 5;
-let b = 12;
-let c = 4.6;
-a *= 5;
-b *= 3;
-c *= 10;//Compound Assignment With Augmented Multiplication
+let a2 = 5;
+let b2 = 12;
+let c2 = 4.6;
+a2 *= 5;
+b2 *= 3;
+c2 *= 10;//Compound Assignment With Augmented Multiplication
 
-let a = 48;
-let b = 108;
-let c = 33;
-a /= 12;
-b /= 4;
-c /= 11;//Compound Assignment With Augmented Division
+let a3 = 48;
+let b3 = 108;
+let c3 = 33;
+a3 /= 12;
+b3 /= 4;
+c3 /= 11;//Compound Assignment With Augmented Division
 
 const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; //Escaping Literal Quotes in Strings
