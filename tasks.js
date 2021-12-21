d183c766e8c7767578e259a9ebd43b3e5c4a0e1b
Vit <vit_waladzko@mail.ru>
2021-12-21 21:24:23 +0300

    21.12/2

--- tasks.js
+++ tasks.js
@@ -1,229 +1,294 @@
-function greet(name){
-    return `Hello, ${name} how are you doing today?`
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`
+}
+console.log(greet('Alex'));
+
+
+function makeNegative(num) {
+  if (num === 0 || num < 0) {
+    return num;
+  } else {
+    return -num
   }
-  console.log(greet('Alex'));
-  
+}
+console.log(makeNegative(1));
+console.log(makeNegative(-5));
+console.log(makeNegative(0));
+console.log(makeNegative(0.12));
 
-  function makeNegative(num) {
-    if(num === 0 || num < 0){
-      return num;
-      }else{
-        return -num
-      }
-    }
-  console.log(makeNegative(1));
-  console.log(makeNegative(-5));
-  console.log(makeNegative(0));
-  console.log(makeNegative(0.12));
-  
 
-  function boolToWord(bool){
-    if (bool===true) {
-      return 'Yes';
-    } else {
-      return 'No';
-    }
+function boolToWord(bool) {
+  if (bool === true) {
+    return 'Yes';
+  } else {
+    return 'No';
   }
-  console.log(boolToWord(true));
-  console.log(boolToWord(false));
+}
+console.log(boolToWord(true));
+console.log(boolToWord(false));
 
-  function opposite(number) {
-    return -number;
+function opposite(number) {
+  return -number;
+}
+console.log(opposite(15));
+console.log(opposite(-5));
+console.log(opposite(17));
+
+function numberToString(num) {
+  return num + '';
+}
+console.log(numberToString(17));
+
+function testEven(n) {
+  if (n % 2 === 0) {
+    return true;
+  } else {
+    return false;
   }
-  console.log(opposite(15));
-  console.log(opposite(-5));
-  console.log(opposite(17));
-
-  function numberToString(num) {
-    return num + '';
-  }
-  console.log(numberToString(17));
-
-  function testEven(n) {
-    if(n%2===0){
-      return true;
-    } else {
-      return false;
-    }
-  }
-  console.log(testEven(0));
-  console.log(testEven(0.5));
-  console.log(testEven(1));
-  console.log(testEven(2));
-  console.log(testEven(-4));
+}
+console.log(testEven(0));
+console.log(testEven(0.5));
+console.log(testEven(1));
+console.log(testEven(2));
+console.log(testEven(-4));
 
 
-  function arrayPlusArray(arr1, arr2) {
-    let arr = arr1.concat(arr2);
-    let sum = arr.reduce(function(sum, elem){
-      return sum += elem;                     
-    })
+function arrayPlusArray(arr1, arr2) {
+  let arr = arr1.concat(arr2);
+  let sum = arr.reduce(function (sum, elem) {
+    return sum += elem;
+  })
   return sum;
+}
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]))//21
+console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6]))//-21
+
+
+
+function even_or_odd(number) {
+  if (number % 2 === 0) {
+    return 'Even';
+  } else {
+    return 'Odd';
   }
-  console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]))//21
-  console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6]))//-21
-
-  
-  
-  function even_or_odd(number) {
-    if(number%2===0){
-      return 'Even';
-    }else{
-      return 'Odd';
-    }
-  }
-  console.log(even_or_odd(1));
-  console.log(even_or_odd(6));
-  console.log(even_or_odd(-71));
-
-  
-  
-  function findSmallestInt(args) {
-      return Math.min.apply(null, args);
-    }
-  console.log(findSmallestInt([-1, -17, -53]));
-  console.log(findSmallestInt([5, -17, 11]));
+}
+console.log(even_or_odd(1));
+console.log(even_or_odd(6));
+console.log(even_or_odd(-71));
 
 
-  function stringToArray(string){
-	return string.split(' ');
+
+function findSmallestInt(args) {
+  return Math.min.apply(null, args);
+}
+console.log(findSmallestInt([-1, -17, -53]));
+console.log(findSmallestInt([5, -17, 11]));
+
+
+function stringToArray(string) {
+  return string.split(' ');
 }
 console.log(stringToArray('Hi world'));
 
 
 function updateLight(current) {
-    if (current === "green"){
-      return "yellow";
-    }
-     if (current === "yellow"){
-      return "red";
-    }
-     if (current === "red"){
-      return "green";
-    }
-   }
-   console.log(updateLight('green'));
-   console.log(updateLight('yellow'));
-   console.log(updateLight('red'));
-
-
-   function reverseWords(str){
-    return str.split(' ').reverse().join(' ');
+  if (current === "green") {
+    return "yellow";
   }
-  console.log(reverseWords('hello world!'));
+  if (current === "yellow") {
+    return "red";
+  }
+  if (current === "red") {
+    return "green";
+  }
+}
+console.log(updateLight('green'));
+console.log(updateLight('yellow'));
+console.log(updateLight('red'));
 
 
-  function diffBig2(arr) {
-    let first = 0;
-    let second = 0;
-    for(let i = 0; i<arr.length; i++){
-      if(arr[i] >= first){
-        first = arr[i];
-      }
+function reverseWords(str) {
+  return str.split(' ').reverse().join(' ');
+}
+console.log(reverseWords('hello world!'));
+
+
+function diffBig2(arr) {
+  let first = 0;
+  let second = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= first) {
+      first = arr[i];
     }
-    arr.splice(arr.indexOf(first), 1);
-      for(let i = 0; i<arr.length; i++){
-        if(arr[i] >= second){
-          second = arr[i];
-        }
-      }
-    return first - second;
+  }
+  arr.splice(arr.indexOf(first), 1);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= second) {
+      second = arr[i];
     }
+  }
+  return first - second;
+}
 
-    console.log(diffBig2([1, 8, 3]))//5
-    console.log(diffBig2([2, 1]))//1
-    console.log(diffBig2([1, 3, 8]))//5
-    console.log(diffBig2([10, -1, 5]))//5
+console.log(diffBig2([1, 8, 3]))//5
+console.log(diffBig2([2, 1]))//1
+console.log(diffBig2([1, 3, 8]))//5
+console.log(diffBig2([10, -1, 5]))//5
 
-    
-    
-    function getEvenNumbers(numbersArray){
-      let arr = numbersArray.filter(function(elem){
-        if(elem%2===0){
-          return true;
-        } else {
-          return false;
-        }
-      })
-      return arr;
+
+
+function getEvenNumbers(numbersArray) {
+  let arr = numbersArray.filter(function (elem) {
+    if (elem % 2 === 0) {
+      return true;
+    } else {
+      return false;
     }
-    console.log(getEvenNumbers([1,2,3,6,8,10]))
-    console.log(getEvenNumbers([1,2]))
-    console.log(getEvenNumbers([12,14,15]))
-    console.log(getEvenNumbers([13,15]))
+  })
+  return arr;
+}
+console.log(getEvenNumbers([1, 2, 3, 6, 8, 10]))
+console.log(getEvenNumbers([1, 2]))
+console.log(getEvenNumbers([12, 14, 15]))
+console.log(getEvenNumbers([13, 15]))
 
 
-    function highAndLow(numbers){
-      let newArr = [];
-      let arr = numbers.split(' ');
-      arr.map(function(elem){
-        return Number(elem)
-      })
-      newArr.push(Math.max.apply(null, arr), Math.min.apply(null, arr))
-      return newArr.join(' ')
+function highAndLow(numbers) {
+  let newArr = [];
+  let arr = numbers.split(' ');
+  arr.map(function (elem) {
+    return Number(elem)
+  })
+  newArr.push(Math.max.apply(null, arr), Math.min.apply(null, arr))
+  return newArr.join(' ')
+}
+console.log(highAndLow('8 3 -5 42 -1 0 0 -9 4 7 4 -4'))
+
+
+function solution(str) {
+  let word = str.split('').reverse().join('');
+  return word;
+}
+console.log(solution('hello'));
+
+
+
+function removeChar(str) {
+  let word = str.split('');
+  word.pop();
+  word.shift();
+  return word.join('');
+}
+console.log(removeChar('hello'));
+
+
+function fixTheMeerkat(arr) {
+  arr.reverse();
+  return arr;
+}
+console.log(fixTheMeerkat(["bottom", "middle", "top"]));
+
+
+function getAverage(marks) {
+  let mark = marks.reduce(function (sum, elem) {
+    return sum += elem;
+  })
+  return Math.floor(mark / marks.length);
+}
+console.log(getAverage([2, 2, 2, 2]));
+console.log(getAverage([1, 2, 3, 4, 5,]));
+console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2]));
+
+
+
+function min(arr) {
+  arr.sort(function (x, y) {
+    if (x > y) {
+      return 1
+    } else {
+      return -1
     }
-    console.log(highAndLow('8 3 -5 42 -1 0 0 -9 4 7 4 -4'))
+  })
+  return arr[0]
+}
 
-    
-    function solution(str){
-      let word = str.split('').reverse().join('');
-      return word;
+function max(arr) {
+  arr.sort(function (x, y) {
+    if (x < y) {
+      return 1
+    } else {
+      return -1
     }
-    console.log(solution('hello'));
+  })
+  return arr[0]
+}
 
-    
+console.log(min([-52, 56, 30, 29, -54, 0, -110]), -110);
+console.log(min([42, 54, 65, 87, 0]), 0);
+console.log(max([4, 6, 2, 1, 9, 63, -134, 566]), 566);
+console.log(max([5]), 5);
 
-    function removeChar(str){
-      let word = str.split('');
-      word.pop();
-      word.shift();
-      return word.join('');
+
+
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100;
+  }
+  else if (exam > 75 && projects >= 5) {
+    return 90
+  }
+  else if (exam > 50 && projects >= 2) {
+    return 75
+  }
+  else {
+    return 0
+  }
+}
+console.log(finalGrade(85, 6))
+console.log(finalGrade(35, 3))
+
+function paperwork(n, m) {
+  if (n <= 0 || m <= 0) {
+    return 0;
+  }
+  else {
+    return n * m;
+  }
+}
+console.log(paperwork(5, 5))
+console.log(paperwork(-5, 5))
+console.log(paperwork(-5, -5))
+
+function doubleEveryOther(a) {
+  for (let i = 1; i < a.length; i += 2) {
+    a[i] = a[i] * 2
+  }
+  return a;
+}
+console.log(doubleEveryOther([1, 2, 3, 4]))
+
+var stringToNumber = function (str) {
+  return Number(str)
+}
+console.log(stringToNumber('-5'))
+
+function correct(string) {
+  let arr = string.split('')
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+    if (arr[i] === '5') {
+      arr[i] = 'S'
     }
-      console.log(removeChar('hello'));
-
-
-      function fixTheMeerkat(arr) {
-        arr.reverse();
-        return arr;
-      }
-      console.log(fixTheMeerkat(["bottom", "middle", "top"]));
-
-
-      function getAverage(marks){
-        let mark = marks.reduce(function(sum,elem){
-          return sum+=elem;
-        })
-        return Math.floor(mark/marks.length);
-        }
-        console.log(getAverage([2,2,2,2]));
-        console.log(getAverage([1,2,3,4,5,]));
-        console.log(getAverage([1,1,1,1,1,1,1,2]));
-
-        
-        
-        function min (arr){
-          arr.sort(function(x,y){
-            if(x>y){
-              return 1
-            } else {
-              return -1
-            }
-          })
-          return arr[0]
-        }
-        
-        function max (arr){
-          arr.sort(function(x,y){
-            if(x<y){
-              return 1
-            } else {
-              return -1
-            }
-          })
-          return arr[0]
-        }
-
-    console.log(min([-52, 56, 30, 29, -54, 0, -110]), -110);
-    console.log(min([42, 54, 65, 87, 0]), 0);
-    console.log(max([4,6,2,1,9,63,-134,566]), 566);
-    console.log(max([5]), 5);
+    else if (arr[i] === '0') {
+      arr[i] = 'O'
+      console.log('1')
+    }
+    else if (arr[i] === '1') {
+      arr[i] = 'I'
+    }
+  }
+  return arr.join('')
+}
+console.log(correct(("L0ND0N")))
+console.log(correct(("DUBL1N")))
+console.log(correct(("BUDAPE5T")))
