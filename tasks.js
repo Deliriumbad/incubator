afd41249f3bd682ef65ef7940c45aa15cc36c47b
Vit <vit_waladzko@mail.ru>
2021-12-29 19:49:01 +0300

    29.12

--- tasks.js
+++ tasks.js
@@ -626,3 +626,19 @@ function peopleWithAgeDrink(old) {
   return "drink toddy";
 }
 console.log(peopleWithAgeDrink(22));
+
+function basicOp(operation, value1, value2) {
+  if (operation === '+') {
+    return value1 + value2;
+  } else if (operation === '-') {
+    return value1 - value2;
+  } else if (operation === '*') {
+    return value1 * value2;
+  } else if (operation === '/') {
+    return value1 / value2;
+  }
+}
+console.log(basicOp('+', 4, 7));
+console.log(basicOp('-', 15, 18));
+console.log(basicOp('*', 5, 5));
+console.log(basicOp('/', 49, 7));
