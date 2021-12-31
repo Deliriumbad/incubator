53e7059cc08252737b7e5901256925298454b0ed
Vit <vit_waladzko@mail.ru>
2021-12-31 10:55:59 +0300

    31.12

--- tasks.js
+++ tasks.js
@@ -675,3 +675,21 @@ function betterThanAverage(classPoints, yourPoints) {
 }
 console.log(betterThanAverage([2, 3], 5));
 console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75));
+
+const quarterOf = (month) => {
+  if (month === 1 || month === 2 || month === 3) {
+    return 1;
+  }
+  if (month === 4 || month === 5 || month === 6) {
+    return 2;
+  }
+  if (month === 7 || month === 8 || month === 9) {
+    return 3;
+  }
+  if (month === 10 || month === 11 || month === 12) {
+    return 4;
+  }
+}
+console.log(quarterOf(3))
+console.log(quarterOf(8))
+console.log(quarterOf(11))
