f93c20279c0d218b06ca885b14609d547e7b7bbf
Vit <vit_waladzko@mail.ru>
2022-01-03 11:09:48 +0300

    03.01/16

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -92,3 +92,6 @@ const myData = myArray[2][1];//Access Multi-Dimensional Arrays With Indexes
 
 const myArray = [["John", 23], ["cat", 2]];
 myArray.push(['dog', 3]);//Manipulate Arrays With push()
+
+const myArray = [["John", 23], ["cat", 2]];
+let removedFromMyArray = myArray.pop();//Manipulate Arrays With pop()
