53f363f3569ab564827956f721d9b6f4f515bc1a
Vit <vit_waladzko@mail.ru>
2022-01-03 10:27:48 +0300

    03.01/6

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -56,3 +56,6 @@ firstLetterOfLastName = lastName[0]; // Use Bracket Notation to Find the First C
 
 let myStr = "Jello World";
 myStr = "Hello World"; // Understand String Immutability
+
+const lastName = "Lovelace";
+const thirdLetterOfLastName = lastName[2];//Use Bracket Notation to Find the Nth Character in a String
