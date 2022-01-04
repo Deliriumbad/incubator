bf38c7949aefad812768d3471145dfb5cfe2e76b
Vit <vit_waladzko@mail.ru>
2022-01-04 10:08:05 +0300

    04.01/9

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -261,3 +261,14 @@ function testLessThan(val) {
     return "55 or Over";
 }
 testLessThan(10);//omparison with the Less Than Operator
+
+function testLessOrEqual(val) {
+    if (val <= 12) {
+        return "Smaller Than or Equal to 12";
+    }
+    if (val <= 24) {
+        return "Smaller Than or Equal to 24";
+    }
+    return "More Than 24";
+}
+testLessOrEqual(10);//Comparison with the Less Than Or Equal To Operator
