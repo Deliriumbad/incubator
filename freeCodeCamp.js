b51eaf847586232577337fb33e8899c8f5fdfa16
Vit <vit_waladzko@mail.ru>
2022-01-04 10:49:06 +0300

    04.01/15

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -321,3 +321,19 @@ function orderMyLogic(val) {
     }
 }
 orderMyLogic(7);//Logical Order in If Else Statements
+
+function testSize(num) {
+    if (num < 5) {
+        return 'Tiny';
+    } else if (num < 10) {
+        return 'Small';
+    } else if (num < 15) {
+        return 'Medium';
+    } else if (num < 20) {
+        return 'Large';
+    } else {
+        return 'Huge';
+    }
+    return "Change Me";
+}
+testSize(7);//Chaining If Else Statements
