72c94e3f2534b61bf2ca3ef96e19dbf878b09efb
Vit <vit_waladzko@mail.ru>
2022-01-04 22:04:01 +0300

    04.01/36

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -587,3 +587,8 @@ for (let i = 1; i <= 5; i++) {
     myArray.push(i);
 }//Iterate with JavaScript For Loops
 
+const myArray = [];
+for (let i = 1; i < 10; i += 2) {
+    myArray.push(i);
+}//Iterate Odd Numbers With a For Loop
+
