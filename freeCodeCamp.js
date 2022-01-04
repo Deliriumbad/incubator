c2bd576b178f68fa83bf90b09943d61775a14837
Vit <vit_waladzko@mail.ru>
2022-01-04 10:23:15 +0300

    04.01/13

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -299,3 +299,14 @@ function testElse(val) {
     return result;
 }
 testElse(4);//Introducing Else Statements
+
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater than 10";
+    } else if (val < 5) {
+        return "Smaller than 5";
+    } else {
+        return "Between 5 and 10";
+    }
+}
+testElseIf(7);//Introducing Else If Statements
