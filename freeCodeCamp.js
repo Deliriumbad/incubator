19ac3438d0d5953b901d650678317254f26a3e34
Vit <vit_waladzko@mail.ru>
2022-01-04 11:01:01 +0300

    04.01/16

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -337,3 +337,21 @@ function testSize(num) {
     return "Change Me";
 }
 testSize(7);//Chaining If Else Statements
+
+function caseInSwitch(val) {
+    let answer = '';
+    switch (val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+}//Selecting from Many Options with Switch Statements
