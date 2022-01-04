a0a87ba669d2eae0374e5b0156fc988762d0351f
Vit <vit_waladzko@mail.ru>
2022-01-04 18:58:52 +0300

    04.01/8

--- tasks.js
+++ tasks.js
@@ -967,3 +967,16 @@ function maps(x) {
 console.log(maps([1, 2, 3]));
 console.log(maps([4, 1, 1, 1, 4]));
 console.log(maps([2, 2, 2, 2, 2, 2]));
+
+function strCount(str, letter) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === letter) {
+      count++
+    }
+  }
+  return count;
+}
+console.log(strCount('Hello', 'o'));
+console.log(strCount('Hello', 'l'));
+console.log(strCount('', 'z'));
