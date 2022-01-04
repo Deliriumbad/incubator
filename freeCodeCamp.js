433fb5ca87f85b1f3f46f9f58e77e490273426ec
Vit <vit_waladzko@mail.ru>
2022-01-04 22:12:31 +0300

    04.01/38

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -597,3 +597,9 @@ for (let i = 9; i > 0; i -= 2) {
     myArray.push(i);
 }//Count Backwards With a For Loop
 
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+    total += myArr[i];
+}//Iterate Through an Array with a For Loop
+
