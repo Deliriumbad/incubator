bc7b8d686873ad83b088f1a616ac5194904d51ce
Vit <vit_waladzko@mail.ru>
2022-01-05 20:01:25 +0300

    05.01/8

--- tasks.js
+++ tasks.js
@@ -1082,3 +1082,16 @@ var TempleStrings = function (obj, feature) {
   return obj + ' are ' + feature;
 }
 console.log(TempleStrings("Animals", "Good"));
+
+function divisibleBy(numbers, divisor) {
+  let arr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % divisor === 0) {
+      arr.push(numbers[i]);
+    }
+  }
+  return arr;
+}
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2), [2, 4, 6]);
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 3), [3, 6]);
+console.log(divisibleBy([0, 1, 2, 3, 4, 5, 6], 4), [0, 4]);
