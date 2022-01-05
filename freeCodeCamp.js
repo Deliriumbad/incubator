f91c25f11fb948fea2e21693b192757aa5524c56
Vit <vit_waladzko@mail.ru>
2022-01-05 13:07:51 +0300

    05.01/4

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -638,4 +638,8 @@ function randomWholeNum() {
     return Math.floor(Math.random() * 5);
 }//Generate Random Whole Numbers with JavaScript
 
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}//Generate Random Whole Numbers within a Range
+
 
