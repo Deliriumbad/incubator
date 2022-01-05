99e84e6dd1fc9796a6fc3e73d2980c2cd4d8bcf8
Vit <vit_waladzko@mail.ru>
2022-01-05 08:52:19 +0300

    05.01/2

--- tasks.js
+++ tasks.js
@@ -1028,3 +1028,16 @@ function sumMix(x) {
   let res = arr.reduce((sum, elem) => sum += elem);
   return res;
 }
+
+function fakeBin(x) {
+  let arr = x.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < '5') {
+      arr[i] = '0';
+    } else {
+      arr[i] = '1';
+    }
+  }
+  return arr.join('');
+}
+console.log(fakeBin('45385593107843568'));
